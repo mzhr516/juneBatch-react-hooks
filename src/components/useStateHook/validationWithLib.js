78e0944d7bf6207811ed215a2d87a519af81d8c7
mohdmazharuddin <106807606+mohdmazharuddin@users.noreply.github.com>
2023-07-04 13:47:50 +0530

    full validation

--- src/components/useStateHook/validationWithLib.js
+++ src/components/useStateHook/validationWithLib.js
@@ -8,12 +8,8 @@ export const ValidationWithLibrary = () => {
     handleSubmit,
     formState: { errors, isSubmitSuccessful },
     reset,
-  } = useForm();
-  console.log(
-    register("name", {
-      required: { value: true, message: "please enter your name" },
-    })
-  );
+  } = useForm({ reValidateMode: "onSubmit" });
+
   const submit = (data) => {
     console.log(data);
     reset();
@@ -34,12 +30,28 @@ export const ValidationWithLibrary = () => {
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>age</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter Name"
+            {...register("age", {
+              min: { value: 18, message: "age not be less than 18" },
+              max: { value: 45, message: "age not be greater than 45" },
+            })}
+          />
+          <p style={{ color: "red" }}>{errors.age?.message}</p>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter email"
             {...register("email", {
               required: { value: true, message: "please enter your email id" },
+              pattern: {
+                value: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+                message: "invalidate email",
+              },
             })}
           />
           <p style={{ color: "red" }}>{errors.email?.message}</p>
@@ -52,6 +64,11 @@ export const ValidationWithLibrary = () => {
             placeholder="Password"
             {...register("password", {
               required: { value: true, message: "please fill password field" },
+              minLength: { value: 5, message: "length must be greater than 5" },
+              maxLength: {
+                value: 10,
+                message: "length not be greater than 10",
+              },
             })}
           />
           <p style={{ color: "red" }}>{errors.password?.message}</p>
